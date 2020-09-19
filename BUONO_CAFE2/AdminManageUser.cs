9e485846fb882ef4e30e07248d649589e62a2a18
DNoel25 <66959341+DNoel25@users.noreply.github.com>
2020-09-19 20:48:15 +0530

    Update AdminManageUser.cs

--- BUONO_CAFE2/AdminManageUser.cs
+++ BUONO_CAFE2/AdminManageUser.cs
@@ -33,5 +33,12 @@ namespace BUONO_CAFE2
         {
 
         }
+
+        private void btnSeachUser_Click(object sender, EventArgs e)
+        {
+            SearchUser s = new SearchUser();
+            s.Show();
+            this.Hide();
+        }
     }
 }
