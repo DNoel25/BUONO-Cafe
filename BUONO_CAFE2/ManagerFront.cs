b175e22f72236799d799ad388e2e90fee1fdfb5e
DNoel25 <66959341+DNoel25@users.noreply.github.com>
2020-09-27 18:35:32 +0530

    Create ManagerFront.cs

---
+++ BUONO_CAFE2/ManagerFront.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BUONO_CAFE2
+{
+    public partial class ManagerFront : Form
+    {
+        public ManagerFront()
+        {
+            InitializeComponent();
+        }
+
+        private void btnViewAccounts_Click(object sender, EventArgs e)
+        {
+            AdminManageUser amu = new AdminManageUser();
+            amu.Show();
+            this.Hide();
+        }
+
+        private void ManagerFront_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnViewStocks_Click(object sender, EventArgs e)
+        {
+            Stocks s = new Stocks();
+            s.Show();
+            this.Hide();
+        }
+
+        private void btnBack_Click(object sender, EventArgs e)
+        {
+            Login l = new Login();
+            l.Show();
+            this.Hide();
+        }
+    }
+}
