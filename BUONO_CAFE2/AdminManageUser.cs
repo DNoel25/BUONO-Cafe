17b37ed012595b0f83a3091161a446510a0461ec
DNoel25 <66959341+DNoel25@users.noreply.github.com>
2020-09-27 18:33:08 +0530

    Update AdminManageUser.cs

--- BUONO_CAFE2/AdminManageUser.cs
+++ BUONO_CAFE2/AdminManageUser.cs
@@ -21,12 +21,14 @@ namespace BUONO_CAFE2
         {
             Add_User a = new Add_User("Add");
             a.Show();
+            this.Hide();
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
             Add_User a = new Add_User("Update");
             a.Show();
+            this.Hide();
         }
 
         private void AdminManageUser_Load(object sender, EventArgs e)
@@ -38,6 +40,13 @@ namespace BUONO_CAFE2
         {
             SearchAndDeleteUser s = new SearchAndDeleteUser();
             s.Show();
+            this.Hide();
+        }
+
+        private void btnBack_Click(object sender, EventArgs e)
+        {
+            Login l = new Login();
+            l.Show();
             this.Hide();
         }
     }
