ff7df00d0ec409405448fdb96346c6de01af9ed9
DNoel25 <66959341+DNoel25@users.noreply.github.com>
2020-10-15 15:06:18 +0530

    Create Units.cs

---
+++ BUONO_CAFE2/Units.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace BUONO_CAFE2
+{
+    public partial class Units : Form
+    {
+        SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;Initial Catalog=BUONO_CAFE;Integrated Security=True");
+
+        public Units()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAddUnit_Click(object sender, EventArgs e)
+        {
+            int count = 0;
+            SqlCommand cmd1 = con.CreateCommand();
+            cmd1.CommandType = CommandType.Text;
+            cmd1.CommandText = "Select * from Unit where unit = '"+txtUnit.Text+"'";
+            cmd1.ExecuteNonQuery();
+            DataTable dt1 = new DataTable();
+            SqlDataAdapter da1 = new SqlDataAdapter(cmd1);
+            da1.Fill(dt1); 
+             
+            count = Convert.ToInt32(dt1.Rows.Count.ToString());
+            if(count == 0)
+            {
+                SqlCommand cmd = con.CreateCommand();
+                cmd.CommandType = CommandType.Text;
+                cmd.CommandText = "insert into Unit values('" + txtUnit.Text + "')";
+                cmd.ExecuteNonQuery();
+                disp();
+                MessageBox.Show("Successfully added");
+            }
+            else
+            {
+                MessageBox.Show("This unit already added");
+            }
+
+            
+        }
+
+        private void Units_Load(object sender, EventArgs e)
+        {
+            if(con.State == ConnectionState.Open)
+            {
+                con.Close();
+            }
+            else
+            {
+                con.Open();
+                disp();
+            }
+        }
+
+        public void disp()
+        {
+            SqlCommand cmd = con.CreateCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "Select * from Unit";
+            cmd.ExecuteNonQuery(); 
+            DataTable dt = new DataTable();
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            da.Fill(dt);
+            dataGridView1.DataSource = dt;
+
+        }
+
+        private void btnDeleteUnit_Click(object sender, EventArgs e)
+        {
+            int id;
+            id = Convert.ToInt32(dataGridView1.SelectedCells[0].Value.ToString());
+            SqlCommand cmd = con.CreateCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "DELETE from Unit where id = '" + id + "'";
+            cmd.ExecuteNonQuery();
+            disp();
+        }
+    }
+}
