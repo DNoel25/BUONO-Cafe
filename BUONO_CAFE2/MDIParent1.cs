23488c1254d2d99318c601ff2a5b1fd6c0204c59
DNoel25 <66959341+DNoel25@users.noreply.github.com>
2020-11-09 12:54:15 +0530

    Update MDIParent1.cs

--- BUONO_CAFE2/MDIParent1.cs
+++ BUONO_CAFE2/MDIParent1.cs
@@ -2,10 +2,12 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web.UI.WebControls;
 using System.Windows.Forms;
 
 namespace BUONO_CAFE2
@@ -13,6 +15,8 @@ namespace BUONO_CAFE2
     public partial class MDIParent1 : Form
     {
         private int childFormNumber = 0;
+        public static string Uleavel = "cashier";
+        public static string UleavelManager = "manager";
 
         public MDIParent1()
         {
@@ -96,11 +100,97 @@ namespace BUONO_CAFE2
         }
 
         private void MDIParent1_Load(object sender, EventArgs e)
-        { 
+        {
+
+
+            label1.Text = Login.SetValueForText1;
+            lbluserpower.Text = Login.SetValueForText2;
+           
+            if (Uleavel == lbluserpower.Text)
+            {
+                usersToolStripMenuItem.Visible = false;
+            }
 
         }
 
         private void usersToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void addUnitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Units un = new Units();
+            un.Show();
+
+        }
+
+        private void deleteInforToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Dealer_Info di = new Dealer_Info();
+            di.Show();
+        }
+
+        private void purchaseToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Purchase p = new Purchase();
+            p.Show();
+        }
+
+        private void purchaseReportToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Purchase_Report pr = new Purchase_Report();
+            pr.Show();
+        }
+
+        private void addProductToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            StockInsert ii = new StockInsert();
+            ii.Show();
+        }
+
+        private void addUserToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            AdminManageUser am = new AdminManageUser();
+            am.Show();
+        }
+
+        private void productToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void searchEmployeeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            AdminManageUser frm2 = new AdminManageUser();
+            frm2.Show();
+            
+        }
+
+        //private void sadasToolStripMenuItem_Click(object sender, EventArgs e)
+        //{
+        //    Form1 frm2 = new Form1();
+        //    frm2.Show();
+        //}
+
+        private void logOutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Dispose();
+
+        }
+
+        private void updateEmployeeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            AdminManageUser am = new AdminManageUser();
+            am.Show();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void unitToolStripMenuItem_Click(object sender, EventArgs e)
         {
 
         }
