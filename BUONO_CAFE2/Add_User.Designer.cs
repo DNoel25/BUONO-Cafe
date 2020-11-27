792102006a703a2fff9da1d60c96cbfbfc67a4e2
DNoel25 <66959341+DNoel25@users.noreply.github.com>
2020-11-27 11:29:21 +0530

    Update Add_User.Designer.cs

--- BUONO_CAFE2/Add_User.Designer.cs
+++ BUONO_CAFE2/Add_User.Designer.cs
@@ -54,197 +54,254 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(120, 60);
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.Window;
+            this.label1.Location = new System.Drawing.Point(165, 75);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(57, 17);
+            this.label1.Size = new System.Drawing.Size(74, 20);
             this.label1.TabIndex = 0;
             this.label1.Text = "User_id";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(120, 100);
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.Window;
+            this.label2.Location = new System.Drawing.Point(165, 125);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(45, 17);
+            this.label2.Size = new System.Drawing.Size(57, 20);
             this.label2.TabIndex = 1;
             this.label2.Text = "Name";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(120, 147);
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.Window;
+            this.label3.Location = new System.Drawing.Point(165, 184);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(69, 17);
+            this.label3.Size = new System.Drawing.Size(91, 20);
             this.label3.TabIndex = 2;
             this.label3.Text = "Password";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(120, 352);
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.SystemColors.Window;
+            this.label4.Location = new System.Drawing.Point(165, 440);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(40, 17);
+            this.label4.Size = new System.Drawing.Size(49, 20);
             this.label4.TabIndex = 3;
             this.label4.Text = "Type";
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(120, 198);
+            this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.SystemColors.Window;
+            this.label5.Location = new System.Drawing.Point(165, 248);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(42, 17);
+            this.label5.Size = new System.Drawing.Size(56, 20);
             this.label5.TabIndex = 4;
             this.label5.Text = "Email";
             // 
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(120, 249);
+            this.label6.BackColor = System.Drawing.Color.Transparent;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.SystemColors.Window;
+            this.label6.Location = new System.Drawing.Point(165, 311);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(30, 17);
+            this.label6.Size = new System.Drawing.Size(40, 20);
             this.label6.TabIndex = 5;
             this.label6.Text = "NIC";
             // 
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(120, 301);
+            this.label7.BackColor = System.Drawing.Color.Transparent;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.ForeColor = System.Drawing.SystemColors.Window;
+            this.label7.Location = new System.Drawing.Point(165, 376);
+            this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(130, 17);
+            this.label7.Size = new System.Drawing.Size(166, 20);
             this.label7.TabIndex = 6;
             this.label7.Text = "Telephone Number";
             // 
             // btnAddUser
             // 
-            this.btnAddUser.Location = new System.Drawing.Point(193, 438);
+            this.btnAddUser.BackColor = System.Drawing.Color.Black;
+            this.btnAddUser.ForeColor = System.Drawing.SystemColors.Window;
+            this.btnAddUser.Location = new System.Drawing.Point(265, 548);
+            this.btnAddUser.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnAddUser.Name = "btnAddUser";
-            this.btnAddUser.Size = new System.Drawing.Size(75, 23);
+            this.btnAddUser.Size = new System.Drawing.Size(103, 29);
             this.btnAddUser.TabIndex = 7;
             this.btnAddUser.Text = "ADD";
-            this.btnAddUser.UseVisualStyleBackColor = true;
+            this.btnAddUser.UseVisualStyleBackColor = false;
             this.btnAddUser.Click += new System.EventHandler(this.btnAddUser_Click);
             // 
             // txtUser_id
             // 
-            this.txtUser_id.Location = new System.Drawing.Point(283, 60);
+            this.txtUser_id.Location = new System.Drawing.Point(389, 75);
+            this.txtUser_id.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtUser_id.Name = "txtUser_id";
-            this.txtUser_id.Size = new System.Drawing.Size(255, 22);
+            this.txtUser_id.Size = new System.Drawing.Size(349, 27);
             this.txtUser_id.TabIndex = 8;
             // 
             // txtName
             // 
-            this.txtName.Location = new System.Drawing.Point(283, 100);
+            this.txtName.Location = new System.Drawing.Point(389, 125);
+            this.txtName.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtName.Name = "txtName";
-            this.txtName.Size = new System.Drawing.Size(255, 22);
+            this.txtName.Size = new System.Drawing.Size(349, 27);
             this.txtName.TabIndex = 9;
             // 
             // txtPassword
             // 
-            this.txtPassword.Location = new System.Drawing.Point(283, 142);
+            this.txtPassword.Location = new System.Drawing.Point(389, 178);
+            this.txtPassword.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtPassword.Name = "txtPassword";
             this.txtPassword.PasswordChar = '*';
-            this.txtPassword.Size = new System.Drawing.Size(255, 22);
+            this.txtPassword.Size = new System.Drawing.Size(349, 27);
             this.txtPassword.TabIndex = 10;
             // 
             // txtEmail
             // 
-            this.txtEmail.Location = new System.Drawing.Point(283, 195);
+            this.txtEmail.Location = new System.Drawing.Point(389, 244);
+            this.txtEmail.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtEmail.Name = "txtEmail";
-            this.txtEmail.Size = new System.Drawing.Size(255, 22);
+            this.txtEmail.Size = new System.Drawing.Size(349, 27);
             this.txtEmail.TabIndex = 12;
             // 
             // txtNIC
             // 
-            this.txtNIC.Location = new System.Drawing.Point(283, 246);
+            this.txtNIC.Location = new System.Drawing.Point(389, 308);
+            this.txtNIC.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtNIC.Name = "txtNIC";
-            this.txtNIC.Size = new System.Drawing.Size(255, 22);
+            this.txtNIC.Size = new System.Drawing.Size(349, 27);
             this.txtNIC.TabIndex = 13;
             // 
             // txtTNo
             // 
-            this.txtTNo.Location = new System.Drawing.Point(283, 296);
+            this.txtTNo.Location = new System.Drawing.Point(389, 370);
+            this.txtTNo.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtTNo.Name = "txtTNo";
-            this.txtTNo.Size = new System.Drawing.Size(255, 22);
+            this.txtTNo.Size = new System.Drawing.Size(349, 27);
             this.txtTNo.TabIndex = 14;
             // 
             // btnFormClear
             // 
-            this.btnFormClear.Location = new System.Drawing.Point(283, 398);
+            this.btnFormClear.BackColor = System.Drawing.Color.Black;
+            this.btnFormClear.ForeColor = System.Drawing.SystemColors.Window;
+            this.btnFormClear.Location = new System.Drawing.Point(389, 498);
+            this.btnFormClear.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnFormClear.Name = "btnFormClear";
-            this.btnFormClear.Size = new System.Drawing.Size(75, 23);
+            this.btnFormClear.Size = new System.Drawing.Size(103, 29);
             this.btnFormClear.TabIndex = 15;
             this.btnFormClear.Text = "CLEAR";
-            this.btnFormClear.UseVisualStyleBackColor = true;
+            this.btnFormClear.UseVisualStyleBackColor = false;
             this.btnFormClear.Click += new System.EventHandler(this.btnFormClear_Click);
             // 
             // btnUpdateUser
             // 
-            this.btnUpdateUser.Location = new System.Drawing.Point(374, 438);
+            this.btnUpdateUser.BackColor = System.Drawing.Color.Black;
+            this.btnUpdateUser.ForeColor = System.Drawing.SystemColors.Window;
+            this.btnUpdateUser.Location = new System.Drawing.Point(514, 548);
+            this.btnUpdateUser.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnUpdateUser.Name = "btnUpdateUser";
-            this.btnUpdateUser.Size = new System.Drawing.Size(75, 23);
+            this.btnUpdateUser.Size = new System.Drawing.Size(103, 29);
             this.btnUpdateUser.TabIndex = 16;
             this.btnUpdateUser.Text = "UPDATE";
-            this.btnUpdateUser.UseVisualStyleBackColor = true;
+            this.btnUpdateUser.UseVisualStyleBackColor = false;
             this.btnUpdateUser.Click += new System.EventHandler(this.btnUpdateUser_Click);
             // 
             // btnBack
             // 
-            this.btnBack.Location = new System.Drawing.Point(283, 438);
+            this.btnBack.BackColor = System.Drawing.Color.Black;
+            this.btnBack.ForeColor = System.Drawing.SystemColors.Window;
+            this.btnBack.Location = new System.Drawing.Point(389, 548);
+            this.btnBack.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnBack.Name = "btnBack";
-            this.btnBack.Size = new System.Drawing.Size(75, 23);
+            this.btnBack.Size = new System.Drawing.Size(103, 29);
             this.btnBack.TabIndex = 20;
             this.btnBack.Text = "BACK";
-            this.btnBack.UseVisualStyleBackColor = true;
+            this.btnBack.UseVisualStyleBackColor = false;
             this.btnBack.Click += new System.EventHandler(this.btnBack_Click);
             // 
             // rdoAdmin
             // 
             this.rdoAdmin.AutoSize = true;
-            this.rdoAdmin.Location = new System.Drawing.Point(283, 347);
+            this.rdoAdmin.BackColor = System.Drawing.Color.Transparent;
+            this.rdoAdmin.ForeColor = System.Drawing.Color.Red;
+            this.rdoAdmin.Location = new System.Drawing.Point(389, 434);
+            this.rdoAdmin.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.rdoAdmin.Name = "rdoAdmin";
-            this.rdoAdmin.Size = new System.Drawing.Size(68, 21);
+            this.rdoAdmin.Size = new System.Drawing.Size(82, 24);
             this.rdoAdmin.TabIndex = 21;
             this.rdoAdmin.TabStop = true;
             this.rdoAdmin.Text = "Admin";
-            this.rdoAdmin.UseVisualStyleBackColor = true;
+            this.rdoAdmin.UseVisualStyleBackColor = false;
             this.rdoAdmin.CheckedChanged += new System.EventHandler(this.rdoAdmin_CheckedChanged);
             // 
             // rdoManager
             // 
             this.rdoManager.AutoSize = true;
-            this.rdoManager.Location = new System.Drawing.Point(399, 347);
+            this.rdoManager.BackColor = System.Drawing.Color.Transparent;
+            this.rdoManager.ForeColor = System.Drawing.Color.Green;
+            this.rdoManager.Location = new System.Drawing.Point(549, 434);
+            this.rdoManager.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.rdoManager.Name = "rdoManager";
-            this.rdoManager.Size = new System.Drawing.Size(85, 21);
+            this.rdoManager.Size = new System.Drawing.Size(102, 24);
             this.rdoManager.TabIndex = 22;
             this.rdoManager.TabStop = true;
             this.rdoManager.Text = "Manager";
-            this.rdoManager.UseVisualStyleBackColor = true;
+            this.rdoManager.UseVisualStyleBackColor = false;
             this.rdoManager.CheckedChanged += new System.EventHandler(this.rdoManager_CheckedChanged);
             // 
             // rdoCashier
             // 
             this.rdoCashier.AutoSize = true;
-            this.rdoCashier.Location = new System.Drawing.Point(515, 347);
+            this.rdoCashier.BackColor = System.Drawing.Color.Transparent;
+            this.rdoCashier.ForeColor = System.Drawing.Color.Green;
+            this.rdoCashier.Location = new System.Drawing.Point(708, 434);
+            this.rdoCashier.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.rdoCashier.Name = "rdoCashier";
-            this.rdoCashier.Size = new System.Drawing.Size(77, 21);
+            this.rdoCashier.Size = new System.Drawing.Size(95, 24);
             this.rdoCashier.TabIndex = 23;
             this.rdoCashier.TabStop = true;
             this.rdoCashier.Text = "Cashier";
-            this.rdoCashier.UseVisualStyleBackColor = true;
+            this.rdoCashier.UseVisualStyleBackColor = false;
             this.rdoCashier.CheckedChanged += new System.EventHandler(this.rdoCashier_CheckedChanged);
             // 
             // txtUserLevel
             // 
-            this.txtUserLevel.Location = new System.Drawing.Point(652, 420);
+            this.txtUserLevel.Location = new System.Drawing.Point(896, 525);
+            this.txtUserLevel.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtUserLevel.Name = "txtUserLevel";
-            this.txtUserLevel.Size = new System.Drawing.Size(28, 22);
+            this.txtUserLevel.Size = new System.Drawing.Size(37, 27);
             this.txtUserLevel.TabIndex = 24;
             // 
             // Add_User
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 505);
+            this.BackgroundImage = global::BUONO_CAFE2.Properties.Resources.Login;
+            this.ClientSize = new System.Drawing.Size(1100, 631);
             this.Controls.Add(this.txtUserLevel);
             this.Controls.Add(this.rdoCashier);
             this.Controls.Add(this.rdoManager);
@@ -266,6 +323,8 @@
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Add_User";
             this.Text = "Add_User";
             this.Load += new System.EventHandler(this.Add_User_Load);
