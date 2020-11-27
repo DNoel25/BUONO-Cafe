7811944fd8d521461b84c52654a0e62a5c7dcd1d
DNoel25 <66959341+DNoel25@users.noreply.github.com>
2020-11-27 11:27:20 +0530

    Create Purchase.Designer.cs

---
+++ BUONO_CAFE2/Purchase.Designer.cs
@@ -0,0 +1,363 @@
+﻿namespace BUONO_CAFE2
+{
+    partial class Purchase
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.cmbProductName = new System.Windows.Forms.ComboBox();
+            this.txtProductQuantity = new System.Windows.Forms.TextBox();
+            this.txtProductPrice = new System.Windows.Forms.TextBox();
+            this.txtProductTotal = new System.Windows.Forms.TextBox();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.cmbDealerName = new System.Windows.Forms.ComboBox();
+            this.cmbPurchaseType = new System.Windows.Forms.ComboBox();
+            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
+            this.txtProfit = new System.Windows.Forms.TextBox();
+            this.lblUnit = new System.Windows.Forms.Label();
+            this.btnPurchase = new System.Windows.Forms.Button();
+            this.label10 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.Window;
+            this.label1.Location = new System.Drawing.Point(158, 165);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(128, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Product Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.Window;
+            this.label2.Location = new System.Drawing.Point(470, 165);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(144, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "ProductQuantity";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.Window;
+            this.label3.Location = new System.Drawing.Point(158, 211);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(124, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Product Price";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.SystemColors.Window;
+            this.label4.Location = new System.Drawing.Point(470, 211);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(122, 20);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Product Total";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.SystemColors.Window;
+            this.label5.Location = new System.Drawing.Point(158, 261);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(134, 20);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Purchase Date";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.Color.Transparent;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.SystemColors.Window;
+            this.label6.Location = new System.Drawing.Point(158, 306);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(119, 20);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Dealer Name";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.BackColor = System.Drawing.Color.Transparent;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.ForeColor = System.Drawing.SystemColors.Window;
+            this.label7.Location = new System.Drawing.Point(158, 360);
+            this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(134, 20);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "Purchase Type";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.BackColor = System.Drawing.Color.Transparent;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.ForeColor = System.Drawing.SystemColors.Window;
+            this.label8.Location = new System.Drawing.Point(158, 415);
+            this.label8.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(107, 20);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "Expiry Date";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.BackColor = System.Drawing.Color.Transparent;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.ForeColor = System.Drawing.SystemColors.Window;
+            this.label9.Location = new System.Drawing.Point(158, 460);
+            this.label9.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(77, 20);
+            this.label9.TabIndex = 8;
+            this.label9.Text = "Profit %";
+            // 
+            // cmbProductName
+            // 
+            this.cmbProductName.AccessibleDescription = " ";
+            this.cmbProductName.ForeColor = System.Drawing.Color.Black;
+            this.cmbProductName.FormattingEnabled = true;
+            this.cmbProductName.Location = new System.Drawing.Point(302, 161);
+            this.cmbProductName.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.cmbProductName.Name = "cmbProductName";
+            this.cmbProductName.Size = new System.Drawing.Size(150, 28);
+            this.cmbProductName.TabIndex = 10;
+            this.cmbProductName.SelectedIndexChanged += new System.EventHandler(this.cmbProductName_SelectedIndexChanged);
+            // 
+            // txtProductQuantity
+            // 
+            this.txtProductQuantity.AccessibleDescription = " ";
+            this.txtProductQuantity.Location = new System.Drawing.Point(615, 161);
+            this.txtProductQuantity.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtProductQuantity.Name = "txtProductQuantity";
+            this.txtProductQuantity.Size = new System.Drawing.Size(124, 27);
+            this.txtProductQuantity.TabIndex = 11;
+            // 
+            // txtProductPrice
+            // 
+            this.txtProductPrice.AccessibleDescription = " ";
+            this.txtProductPrice.Location = new System.Drawing.Point(302, 207);
+            this.txtProductPrice.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtProductPrice.Name = "txtProductPrice";
+            this.txtProductPrice.Size = new System.Drawing.Size(124, 27);
+            this.txtProductPrice.TabIndex = 12;
+            this.txtProductPrice.Leave += new System.EventHandler(this.txtProductPrice_Leave);
+            // 
+            // txtProductTotal
+            // 
+            this.txtProductTotal.AccessibleDescription = " ";
+            this.txtProductTotal.Location = new System.Drawing.Point(615, 207);
+            this.txtProductTotal.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtProductTotal.Name = "txtProductTotal";
+            this.txtProductTotal.Size = new System.Drawing.Size(124, 27);
+            this.txtProductTotal.TabIndex = 13;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.AccessibleDescription = " ";
+            this.dateTimePicker1.Location = new System.Drawing.Point(302, 255);
+            this.dateTimePicker1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(304, 27);
+            this.dateTimePicker1.TabIndex = 14;
+            // 
+            // cmbDealerName
+            // 
+            this.cmbDealerName.AccessibleDescription = " ";
+            this.cmbDealerName.FormattingEnabled = true;
+            this.cmbDealerName.Location = new System.Drawing.Point(302, 303);
+            this.cmbDealerName.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.cmbDealerName.Name = "cmbDealerName";
+            this.cmbDealerName.Size = new System.Drawing.Size(150, 28);
+            this.cmbDealerName.TabIndex = 15;
+            // 
+            // cmbPurchaseType
+            // 
+            this.cmbPurchaseType.AccessibleDescription = " ";
+            this.cmbPurchaseType.FormattingEnabled = true;
+            this.cmbPurchaseType.Items.AddRange(new object[] {
+            "CASH",
+            "DEBIT"});
+            this.cmbPurchaseType.Location = new System.Drawing.Point(302, 356);
+            this.cmbPurchaseType.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.cmbPurchaseType.Name = "cmbPurchaseType";
+            this.cmbPurchaseType.Size = new System.Drawing.Size(150, 28);
+            this.cmbPurchaseType.TabIndex = 16;
+            // 
+            // dateTimePicker2
+            // 
+            this.dateTimePicker2.AccessibleDescription = " ";
+            this.dateTimePicker2.Location = new System.Drawing.Point(302, 409);
+            this.dateTimePicker2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.dateTimePicker2.Name = "dateTimePicker2";
+            this.dateTimePicker2.Size = new System.Drawing.Size(304, 27);
+            this.dateTimePicker2.TabIndex = 17;
+            // 
+            // txtProfit
+            // 
+            this.txtProfit.AccessibleDescription = " ";
+            this.txtProfit.Location = new System.Drawing.Point(302, 456);
+            this.txtProfit.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtProfit.Name = "txtProfit";
+            this.txtProfit.Size = new System.Drawing.Size(124, 27);
+            this.txtProfit.TabIndex = 18;
+            // 
+            // lblUnit
+            // 
+            this.lblUnit.AutoSize = true;
+            this.lblUnit.BackColor = System.Drawing.Color.Transparent;
+            this.lblUnit.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblUnit.ForeColor = System.Drawing.SystemColors.Window;
+            this.lblUnit.Location = new System.Drawing.Point(759, 211);
+            this.lblUnit.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblUnit.Name = "lblUnit";
+            this.lblUnit.Size = new System.Drawing.Size(43, 20);
+            this.lblUnit.TabIndex = 9;
+            this.lblUnit.Text = "Unit";
+            // 
+            // btnPurchase
+            // 
+            this.btnPurchase.BackColor = System.Drawing.Color.Black;
+            this.btnPurchase.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPurchase.ForeColor = System.Drawing.SystemColors.Window;
+            this.btnPurchase.Location = new System.Drawing.Point(404, 530);
+            this.btnPurchase.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnPurchase.Name = "btnPurchase";
+            this.btnPurchase.Size = new System.Drawing.Size(182, 40);
+            this.btnPurchase.TabIndex = 19;
+            this.btnPurchase.Text = "Purchase Stock";
+            this.btnPurchase.UseVisualStyleBackColor = false;
+            this.btnPurchase.Click += new System.EventHandler(this.btnPurchase_Click);
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.BackColor = System.Drawing.Color.Transparent;
+            this.label10.Font = new System.Drawing.Font("MS Reference Sans Serif", 25.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.ForeColor = System.Drawing.Color.Brown;
+            this.label10.Location = new System.Drawing.Point(236, 32);
+            this.label10.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(530, 55);
+            this.label10.TabIndex = 20;
+            this.label10.Text = "Purchase  need  stocks";
+            this.label10.Click += new System.EventHandler(this.label10_Click);
+            // 
+            // Purchase
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::BUONO_CAFE2.Properties.Resources.Login;
+            this.ClientSize = new System.Drawing.Size(1012, 616);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.btnPurchase);
+            this.Controls.Add(this.txtProfit);
+            this.Controls.Add(this.dateTimePicker2);
+            this.Controls.Add(this.cmbPurchaseType);
+            this.Controls.Add(this.cmbDealerName);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.txtProductTotal);
+            this.Controls.Add(this.txtProductPrice);
+            this.Controls.Add(this.txtProductQuantity);
+            this.Controls.Add(this.cmbProductName);
+            this.Controls.Add(this.lblUnit);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Purchase";
+            this.Text = "Purchase";
+            this.Load += new System.EventHandler(this.Purchase_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.ComboBox cmbProductName;
+        private System.Windows.Forms.TextBox txtProductQuantity;
+        private System.Windows.Forms.TextBox txtProductPrice;
+        private System.Windows.Forms.TextBox txtProductTotal;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.ComboBox cmbDealerName;
+        private System.Windows.Forms.ComboBox cmbPurchaseType;
+        private System.Windows.Forms.DateTimePicker dateTimePicker2;
+        private System.Windows.Forms.TextBox txtProfit;
+        private System.Windows.Forms.Label lblUnit;
+        private System.Windows.Forms.Button btnPurchase;
+        private System.Windows.Forms.Label label10;
+    }
+}
