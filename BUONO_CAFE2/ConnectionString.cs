7ecee979f5426088f09a8ce6bbe4930292a2a00f
DNoel25 <66959341+DNoel25@users.noreply.github.com>
2020-11-27 11:29:02 +0530

    Create ConnectionString.cs

---
+++ BUONO_CAFE2/ConnectionString.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Project_management_system
+{
+    class ConnectionString
+    {
+    //   public string ConDB= "Data Source=.\\SQLEXPRESS;AttachDbFilename=|Datadirectory|\\pms.mdf;Integrated Security=True;Connect Timeout=30;User Instance=True";
+        public string ConDB = "Data Source = (LocalDB)\\MSSQLLocalDB;Initial Catalog = BUONO_CAFE; Integrated Security = True";
+
+
+      //  SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;Initial Catalog=BUONO_CAFE;Integrated Security=True");
+
+    }
+
+
+}
