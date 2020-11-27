99f8879ae494a58957434ef97b8f522e2d0599cc
DNoel25 <66959341+DNoel25@users.noreply.github.com>
2020-11-27 11:27:53 +0530

    Update Login.Designer.cs

--- BUONO_CAFE2/Login.Designer.cs
+++ BUONO_CAFE2/Login.Designer.cs
@@ -36,12 +36,16 @@
             this.btnClear = new System.Windows.Forms.Button();
             this.btnLogin = new System.Windows.Forms.Button();
             this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtUserLevel = new System.Windows.Forms.TextBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(79, 50);
+            this.label1.Location = new System.Drawing.Point(218, 151);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(79, 17);
             this.label1.TabIndex = 0;
@@ -50,7 +54,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(272, 50);
+            this.label2.Location = new System.Drawing.Point(218, 196);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(69, 17);
             this.label2.TabIndex = 1;
@@ -59,7 +63,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(79, 159);
+            this.label3.Location = new System.Drawing.Point(216, 104);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(83, 17);
             this.label3.TabIndex = 2;
@@ -67,25 +71,25 @@
             // 
             // txtUserName
             // 
-            this.txtUserName.Location = new System.Drawing.Point(82, 87);
+            this.txtUserName.Location = new System.Drawing.Point(219, 171);
             this.txtUserName.Name = "txtUserName";
-            this.txtUserName.Size = new System.Drawing.Size(121, 22);
+            this.txtUserName.Size = new System.Drawing.Size(339, 22);
             this.txtUserName.TabIndex = 3;
             // 
             // txtPassword
             // 
             this.txtPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
-            this.txtPassword.Location = new System.Drawing.Point(268, 87);
+            this.txtPassword.Location = new System.Drawing.Point(221, 216);
             this.txtPassword.Name = "txtPassword";
             this.txtPassword.PasswordChar = '*';
-            this.txtPassword.Size = new System.Drawing.Size(127, 23);
+            this.txtPassword.Size = new System.Drawing.Size(337, 23);
             this.txtPassword.TabIndex = 4;
             // 
             // btnClear
             // 
-            this.btnClear.Location = new System.Drawing.Point(275, 211);
+            this.btnClear.Location = new System.Drawing.Point(219, 321);
             this.btnClear.Name = "btnClear";
-            this.btnClear.Size = new System.Drawing.Size(75, 23);
+            this.btnClear.Size = new System.Drawing.Size(339, 33);
             this.btnClear.TabIndex = 7;
             this.btnClear.Text = "Clear";
             this.btnClear.UseVisualStyleBackColor = true;
@@ -93,31 +97,81 @@
             // 
             // btnLogin
             // 
-            this.btnLogin.Location = new System.Drawing.Point(82, 273);
+            this.btnLogin.BackColor = System.Drawing.SystemColors.Window;
+            this.btnLogin.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLogin.ForeColor = System.Drawing.Color.Black;
+            this.btnLogin.Location = new System.Drawing.Point(219, 264);
             this.btnLogin.Name = "btnLogin";
-            this.btnLogin.Size = new System.Drawing.Size(75, 23);
+            this.btnLogin.Size = new System.Drawing.Size(339, 42);
             this.btnLogin.TabIndex = 8;
             this.btnLogin.Text = "Login";
-            this.btnLogin.UseVisualStyleBackColor = true;
+            this.btnLogin.UseVisualStyleBackColor = false;
             this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
             // 
             // comboBox1
             // 
+            this.comboBox1.BackColor = System.Drawing.SystemColors.ActiveBorder;
             this.comboBox1.FormattingEnabled = true;
             this.comboBox1.Items.AddRange(new object[] {
             "admin",
             "manager",
             "cashier"});
-            this.comboBox1.Location = new System.Drawing.Point(82, 211);
+            this.comboBox1.Location = new System.Drawing.Point(219, 124);
             this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(121, 24);
+            this.comboBox1.Size = new System.Drawing.Size(339, 24);
             this.comboBox1.TabIndex = 9;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Mistral", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(211, 23);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(401, 48);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Welcome!! Have a nice day!";
+            // 
+            // txtUserLevel
+            // 
+            this.txtUserLevel.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+            this.txtUserLevel.Location = new System.Drawing.Point(778, 369);
+            this.txtUserLevel.Name = "txtUserLevel";
+            this.txtUserLevel.PasswordChar = '*';
+            this.txtUserLevel.Size = new System.Drawing.Size(10, 23);
+            this.txtUserLevel.TabIndex = 11;
+            this.txtUserLevel.Visible = false;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.progressBar1.Location = new System.Drawing.Point(0, 427);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(800, 23);
+            this.progressBar1.TabIndex = 12;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+            this.textBox1.Location = new System.Drawing.Point(778, 398);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.PasswordChar = '*';
+            this.textBox1.Size = new System.Drawing.Size(10, 23);
+            this.textBox1.TabIndex = 13;
+            this.textBox1.Visible = false;
             // 
             // Login
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Window;
+            this.BackgroundImage = global::BUONO_CAFE2.Properties.Resources.One;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.txtUserLevel);
+            this.Controls.Add(this.label4);
             this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.btnLogin);
             this.Controls.Add(this.btnClear);
@@ -126,7 +180,9 @@
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.Name = "Login";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Login";
             this.Load += new System.EventHandler(this.Login_Load);
             this.ResumeLayout(false);
@@ -143,6 +199,10 @@
         private System.Windows.Forms.TextBox txtPassword;
         private System.Windows.Forms.Button btnClear;
         private System.Windows.Forms.Button btnLogin;
-        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label4;
+        public System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox txtUserLevel;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
