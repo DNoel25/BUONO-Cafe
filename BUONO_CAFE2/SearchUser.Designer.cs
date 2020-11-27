eb013c8a95fd20209a9da8f300207ce5fc5e83d0
DNoel25 <66959341+DNoel25@users.noreply.github.com>
2020-11-27 11:27:00 +0530

    Create SearchUser.Designer.cs

---
+++ BUONO_CAFE2/SearchUser.Designer.cs
@@ -0,0 +1,218 @@
+﻿namespace BUONO_CAFE2
+{
+    partial class SearchUser
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblName = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnsearch2 = new System.Windows.Forms.Button();
+            this.btnsearch1 = new System.Windows.Forms.Button();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtEId = new System.Windows.Forms.TextBox();
+            this.btnBack = new System.Windows.Forms.Button();
+            this.EmployeeDataGridView = new System.Windows.Forms.DataGridView();
+            this.btnSearch3 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtNIC = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.EmployeeDataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.BackColor = System.Drawing.Color.Transparent;
+            this.lblName.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblName.ForeColor = System.Drawing.Color.IndianRed;
+            this.lblName.Location = new System.Drawing.Point(175, 31);
+            this.lblName.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(68, 25);
+            this.lblName.TabIndex = 9;
+            this.lblName.Text = "Name";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.IndianRed;
+            this.label1.Location = new System.Drawing.Point(13, 31);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(84, 25);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "User ID";
+            // 
+            // btnsearch2
+            // 
+            this.btnsearch2.BackColor = System.Drawing.Color.Black;
+            this.btnsearch2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnsearch2.ForeColor = System.Drawing.SystemColors.Window;
+            this.btnsearch2.Location = new System.Drawing.Point(180, 112);
+            this.btnsearch2.Margin = new System.Windows.Forms.Padding(4);
+            this.btnsearch2.Name = "btnsearch2";
+            this.btnsearch2.Size = new System.Drawing.Size(151, 32);
+            this.btnsearch2.TabIndex = 11;
+            this.btnsearch2.Text = "Search";
+            this.btnsearch2.UseVisualStyleBackColor = false;
+            this.btnsearch2.Click += new System.EventHandler(this.btnsearch2_Click);
+            // 
+            // btnsearch1
+            // 
+            this.btnsearch1.BackColor = System.Drawing.Color.Black;
+            this.btnsearch1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnsearch1.ForeColor = System.Drawing.SystemColors.Window;
+            this.btnsearch1.Location = new System.Drawing.Point(13, 112);
+            this.btnsearch1.Margin = new System.Windows.Forms.Padding(4);
+            this.btnsearch1.Name = "btnsearch1";
+            this.btnsearch1.Size = new System.Drawing.Size(129, 32);
+            this.btnsearch1.TabIndex = 10;
+            this.btnsearch1.Text = "Search";
+            this.btnsearch1.UseVisualStyleBackColor = false;
+            this.btnsearch1.Click += new System.EventHandler(this.btnsearch1_Click);
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(180, 72);
+            this.txtName.Margin = new System.Windows.Forms.Padding(4);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(151, 22);
+            this.txtName.TabIndex = 13;
+            // 
+            // txtEId
+            // 
+            this.txtEId.Location = new System.Drawing.Point(13, 72);
+            this.txtEId.Margin = new System.Windows.Forms.Padding(4);
+            this.txtEId.Name = "txtEId";
+            this.txtEId.Size = new System.Drawing.Size(129, 22);
+            this.txtEId.TabIndex = 12;
+            // 
+            // btnBack
+            // 
+            this.btnBack.BackColor = System.Drawing.Color.Black;
+            this.btnBack.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBack.ForeColor = System.Drawing.SystemColors.Window;
+            this.btnBack.Location = new System.Drawing.Point(696, 489);
+            this.btnBack.Margin = new System.Windows.Forms.Padding(4);
+            this.btnBack.Name = "btnBack";
+            this.btnBack.Size = new System.Drawing.Size(188, 32);
+            this.btnBack.TabIndex = 16;
+            this.btnBack.Text = "Go Back";
+            this.btnBack.UseVisualStyleBackColor = false;
+            this.btnBack.Click += new System.EventHandler(this.btnBack_Click);
+            // 
+            // EmployeeDataGridView
+            // 
+            this.EmployeeDataGridView.BackgroundColor = System.Drawing.SystemColors.MenuText;
+            this.EmployeeDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.EmployeeDataGridView.GridColor = System.Drawing.Color.Black;
+            this.EmployeeDataGridView.Location = new System.Drawing.Point(18, 167);
+            this.EmployeeDataGridView.Name = "EmployeeDataGridView";
+            this.EmployeeDataGridView.RowHeadersWidth = 51;
+            this.EmployeeDataGridView.RowTemplate.Height = 24;
+            this.EmployeeDataGridView.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.EmployeeDataGridView.Size = new System.Drawing.Size(866, 228);
+            this.EmployeeDataGridView.TabIndex = 26;
+            // 
+            // btnSearch3
+            // 
+            this.btnSearch3.BackColor = System.Drawing.Color.Black;
+            this.btnSearch3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSearch3.ForeColor = System.Drawing.SystemColors.Window;
+            this.btnSearch3.Location = new System.Drawing.Point(364, 112);
+            this.btnSearch3.Margin = new System.Windows.Forms.Padding(4);
+            this.btnSearch3.Name = "btnSearch3";
+            this.btnSearch3.Size = new System.Drawing.Size(151, 32);
+            this.btnSearch3.TabIndex = 11;
+            this.btnSearch3.Text = "Search";
+            this.btnSearch3.UseVisualStyleBackColor = false;
+            this.btnSearch3.Click += new System.EventHandler(this.btnSearch3_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.IndianRed;
+            this.label2.Location = new System.Drawing.Point(359, 31);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(134, 25);
+            this.label2.TabIndex = 27;
+            this.label2.Text = "Employee ID";
+            // 
+            // txtNIC
+            // 
+            this.txtNIC.Location = new System.Drawing.Point(364, 72);
+            this.txtNIC.Margin = new System.Windows.Forms.Padding(4);
+            this.txtNIC.Name = "txtNIC";
+            this.txtNIC.Size = new System.Drawing.Size(151, 22);
+            this.txtNIC.TabIndex = 28;
+            // 
+            // SearchUser
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.BackgroundImage = global::BUONO_CAFE2.Properties.Resources.Login;
+            this.ClientSize = new System.Drawing.Size(897, 534);
+            this.Controls.Add(this.txtNIC);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnSearch3);
+            this.Controls.Add(this.EmployeeDataGridView);
+            this.Controls.Add(this.btnBack);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.txtEId);
+            this.Controls.Add(this.btnsearch2);
+            this.Controls.Add(this.btnsearch1);
+            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.label1);
+            this.Name = "SearchUser";
+            this.Text = "SearchUser";
+            this.Load += new System.EventHandler(this.SearchUser_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.EmployeeDataGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnsearch2;
+        private System.Windows.Forms.Button btnsearch1;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtEId;
+        private System.Windows.Forms.Button btnBack;
+        private System.Windows.Forms.DataGridView EmployeeDataGridView;
+        private System.Windows.Forms.Button btnSearch3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtNIC;
+    }
+}
